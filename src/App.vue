8aca3e18c411461ccc934c431076934035d2b46f
Corey Bishop <corey.bishop@fool.com>
2020-04-11 12:55:13 -0400

    have ConsoleLine created

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/mask.jpeg"> -->
+      <console-line
+        v-for="line in lines"
+        v-bind:key="line.id"
+        v-bind:line="line.text"
+      ></console-line>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConsoleLine from './components/ConsoleLine.vue'
 
 export default {
   name: 'App',
+  el: '#app',
   components: {
-    HelloWorld
+    ConsoleLine
+  },
+  data: function() {  
+    return { 
+      lines: lines
+    }
   }
 }
+
+var lines = [
+  { id: 1, text: 'My journey with Vue' },
+  { id: 2, text: 'Blogging with Vue' },
+  { id: 3, text: 'Why Vue is so fun' }
+];
+lines.push({ id: 4, text: 'HA' });
+
 </script>
 
 <style>
@@ -22,7 +40,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #4ceb34;
   margin-top: 60px;
+  background-color: black;
 }
 </style>
---
+++ src/components/ConsoleLine.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="typewriter">
+    <h1>{{ line }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'console-line',
+  props: ['line']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.typewriter h1 {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: .15em; /* Adjust as needed */
+  animation: 
+    typing 3.5s steps(40, end),
+    blink-caret .75s step-end infinite;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
+}
+</style>
